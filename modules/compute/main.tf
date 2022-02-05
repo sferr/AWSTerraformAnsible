3175031200f8041fcbf77743d97ccfe730e78add
Simao <simao.ferreira@outlook.com>
2022-02-05 13:17:16 -0500

    sdfsdf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -26,7 +26,8 @@ resource "aws_instance" "webserver" {
   connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = "${file(var.ssh_key_private)}"
+      #private_key = "${file(var.ssh_key_private)}"
+      key_file    = "service_terraform"
       host        = self.public_ip
   }
   
