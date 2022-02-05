a295c60a02d5b2f24bdf6264f9cfe6583e51adbf
Simao <simao.ferreira@outlook.com>
2022-02-04 22:44:04 -0500

    .

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -14,7 +14,7 @@ data "aws_ssm_parameter" "webserver-ami" {
 #======================================
 resource "aws_key_pair" "aws-key" {
   key_name   = "webserver"
-  public_key = file("~/.ssh/${var.service_terraform}.pub")
+  public_key = file("~/.ssh/${service_terraform}.pub")
 }
 
 #Create and bootstrap webserver
